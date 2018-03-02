e74fba09c7b5ed8ead3e88401136e3f596533b3b
RobertGu <gy19941011@gmail.com>
2018-03-02 17:15:16 +0800

    initial commit

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 12;
+    int y = 50;
+
+    cout << "x is " << x << endl;
+    cout << "y is " << y << endl;
+
+    cout << "x is " << x << endl;
+    cout << "y is " << y << endl;
+
+    return 0;
+}
