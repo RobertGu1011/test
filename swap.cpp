e81bf95471eaf61c3e84982bf98fe2b774b8b412
RobertGu <gy19941011@gmail.com>
2018-03-03 23:04:03 +0800

    new change

--- swap.cpp
+++ swap.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int x = 12;
     int y = 50;
-
+    
     cout << "x is " << x << endl;
     cout << "y is " << y << endl;
 
@@ -14,7 +14,7 @@ int main()
     cout << "y is " << y << endl;
 
     cout << "new line" << endl;
-
+    
     cout << "new new line" << endl;
 
     return 0;
